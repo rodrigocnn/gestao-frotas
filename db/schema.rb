6f8d84faf074e1390e86338fb41ffa392ceb6b56
Rodrigo César <rodrigocnn@gmail.com>
2025-06-26 20:27:01 -0300

    feat: create table trips

---
+++ db/schema.rb
@@ -0,0 +1,62 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.2].define(version: 2025_06_22_180001) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "drivers", force: :cascade do |t|
+    t.string "name"
+    t.string "cpf"
+    t.string "cnh"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fleets", force: :cascade do |t|
+    t.string "name"
+    t.string "responsible"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "origin"
+    t.string "destination"
+    t.integer "status"
+    t.bigint "driver_id", null: false
+    t.bigint "vehicle_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["driver_id"], name: "index_trips_on_driver_id"
+    t.index ["vehicle_id"], name: "index_trips_on_vehicle_id"
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "plate"
+    t.string "brand"
+    t.string "model"
+    t.integer "year"
+    t.integer "status"
+    t.bigint "fleet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fleet_id"], name: "index_vehicles_on_fleet_id"
+  end
+
+  add_foreign_key "trips", "drivers"
+  add_foreign_key "trips", "vehicles"
+  add_foreign_key "vehicles", "fleets"
+end
