83234a51ae0da69d16c6a5d56d278db1583d08e4
Rodrigo César <rodrigocnn@gmail.com>
2025-06-26 20:26:04 -0300

    feat: add initial implementation to drivers and vehicles controllers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,38 @@
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+    render json: @drivers
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      render json: @driver, status: :created
+    else
+      render json: @driver.errors, status: :unprocessable_entity
+    end
+  end
+
+    def update
+    if @driver.update(driver_params)
+      render json: @driver
+    else
+      render json: @driver.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @driver.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_driver
+    @driver = Driver.find(params[:id])
+  end
+
+  def driver_params
+    params.require(:driver).permit(:name, :cpf)
+  end
 end
--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,7 @@
 class VehiclesController < ApplicationController
+  def index
+    @vehicles = Vehicle.all
+    @vehicles = @vehicles.filter_by_status(params[:status]) if params[:status].present?
+    render json: @vehicles
+  end
 end
