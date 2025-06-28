c1604a88e44c42f0b8c729fa8ee72ee366200634
Rodrigo César <rodrigocnn@gmail.com>
2025-06-28 12:09:41 -0300

    feat(vehicles/controller) add available_plates method

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -4,4 +4,10 @@ class VehiclesController < ApplicationController
     @vehicles = @vehicles.filter_by_status(params[:status]) if params[:status].present?
     render json: @vehicles
   end
+
+
+  def available_plates
+    @plates = Vehicle.where(status: :disponivel).pluck(:plate)
+    render json: @plates
+  end
 end
