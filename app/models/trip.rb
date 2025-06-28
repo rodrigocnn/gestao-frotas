001ec7dbfdb4ffd5aceeaa70a183315588788f34
Rodrigo César <rodrigocnn@gmail.com>
2025-06-28 11:58:10 -0300

    feat(trip): valida disponibilidade e atualiza status do veículo automaticamente

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,7 +3,33 @@ class Trip < ApplicationRecord
   belongs_to :vehicle
 
   enum status: { planejada: 1, em_andamento: 2, finalizada: 3 }
+
+  before_validation :ensure_vehicle_is_available
+
+  after_create :set_vehicle_status_em_uso
+  after_update :set_vehicle_status_disponivel_if_finalized
+
   validates :start_date, :end_date, :origin, :destination, :status, presence: true
 
   scope :filter_by_status, ->(status_value) { where(status: status_value) }
+
+  private
+
+  def ensure_vehicle_is_available
+    return unless vehicle.present?
+
+    unless vehicle.disponivel?
+      errors.add(:vehicle, "não está disponível para essa viagem")
+    end
+  end
+
+  def set_vehicle_status_em_uso
+    vehicle.update_column(:status, :em_uso)
+  end
+
+  def set_vehicle_status_disponivel_if_finalized
+    if saved_change_to_status? && finalizada?
+      vehicle.update_column(:status, :disponivel)
+    end
+  end
 end
