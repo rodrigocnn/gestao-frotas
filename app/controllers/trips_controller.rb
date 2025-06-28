b9373c5e041d4398478e25a11c031b508d5824c3
Rodrigo César <rodrigocnn@gmail.com>
2025-06-28 11:59:35 -0300

    feat(trip/controller) add methods trip_params , set_trip and create

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,29 @@
 class TripsController < ApplicationController
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      render json: @trip, status: :created
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(
+      :start_date,
+      :end_date,
+      :origin,
+      :destination,
+      :status,
+      :driver_id,
+      :vehicle_id,
+      )
+  end
 end
