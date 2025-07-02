2d0b01de4ddfde5b5a689e32956dfb76e20e2ba9
Rodrigo César <rodrigocnn@gmail.com>
2025-07-02 16:49:34 -0300

    feat:(users) add table user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_22_180001) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_02_120108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,20 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_22_180001) do
     t.datetime "updated_at", null: false
     t.index ["driver_id"], name: "index_trips_on_driver_id"
     t.index ["vehicle_id"], name: "index_trips_on_vehicle_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jti", default: "", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vehicles", force: :cascade do |t|
