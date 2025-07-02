8aec710821793dcb58513035ba1d1b233a429b3b
Rodrigo César <rodrigocnn@gmail.com>
2025-07-02 16:48:22 -0300

    feat(vehicle) add method filtered_vehicles

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,6 +3,17 @@ class Vehicle < ApplicationRecord
   has_many :trips
 
   enum status: { disponivel: 1, em_uso: 2, manutencao: 3 }
+
   validates :plate, :brand, :model, :year, :status, :fleet, presence: true
+
   scope :filter_by_status, ->(status_value) { where(status: status_value) }
+  scope :filter_by_brand, ->(brand) { where(brand: brand) }
+
+  def self.filtered_vehicles(status: nil, brand: nil, year: nil)
+    vehicles = all
+    vehicles = vehicles.filter_by_status(status) if status.present?
+    vehicles = vehicles.filter_by_brand(brand) if brand.present?
+    vehicles = vehicles.order(year: :asc) if year.present?
+    vehicles
+  end
 end
