c8ecc38abf3e5624f9f64a8486321a9226df9633
Rodrigo César <rodrigocnn@gmail.com>
2025-07-02 16:45:17 -0300

    chore: config routes in namespace api V1

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
-  resources :trips
-  resources :drivers
-  resources :vehicles
-  resources :fleets
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+
+ namespace :api do
+    namespace :v1 do
+      resources :trips
+      resources :drivers
+      resources :vehicles
+      resources :fleets
+
+      devise_for :users,
+                 path: "users",
+                 singular: :user,
+                 controllers: {
+                   sessions: "api/v1/users/sessions",
+                   registrations: "api/v1/users/registrations"
+                 },
+                 defaults: { format: :json }
+    end
+  end
 end
