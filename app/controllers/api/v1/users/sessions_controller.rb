a7814d3c0445b4fb492004a5eefbeada94a86208
Rodrigo César <rodrigocnn@gmail.com>
2025-07-05 13:03:25 -0300

    refactor(user/controllers) : update sessions controller

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,24 +1,26 @@
 module Api
   module V1
     module Users
-      class Users::SessionsController < Devise::SessionsController
+      class SessionsController < Devise::SessionsController
         respond_to :json
+
+        # Desativa funcionalidades que dependem de sessão
+
+        skip_before_action :require_no_authentication, only: [ :create ]
 
 
         private
 
         def respond_with(resource, _opts = {})
-          # Usamos o método `encode` para gerar o JWT
-          token = Warden::JWTAuth::UserEncoder.new.call(resource, :user, nil)
+          token, _payload = Warden::JWTAuth::UserEncoder.new.call(resource, :user, nil)
 
           render json: {
             user: resource,
-            token: token.first,  # Aqui, pegamos o primeiro elemento do array
+            token: token,
             message: "Login realizado com sucesso."
           }, status: :ok
         end
 
-        # Para o logout, apenas responde com uma mensagem de sucesso
         def respond_to_on_destroy
           render json: { message: "Logout realizado com sucesso." }, status: :ok
         end
