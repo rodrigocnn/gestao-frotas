0979422b6ee22c642b9d0a2323f8547aa3fbfeb1
Rodrigo César <rodrigocnn@gmail.com>
2025-07-05 12:55:55 -0300

    test(users/factories) add implementation factory to create user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.unique.email }
+    role { "admin" }
+    password { "123456" }
+    password_confirmation { "123456" }
   end
 end
