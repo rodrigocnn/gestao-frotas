93cc5715e5f0b244407d62bc9a79d6455da4cb24
Rodrigo César <rodrigocnn@gmail.com>
2025-07-05 12:58:08 -0300

    feat(controller) add config to use Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::API
+  include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    render json: { error: "Você não tem permissão para isso." }, status: :forbidden
+  end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password, :password_confirmation ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password, :password_confirmation, :role ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :email, :password, :password_confirmation, :role ])
   end
 end
