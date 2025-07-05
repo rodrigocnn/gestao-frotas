6db0dde2e9f4a6c1fe9a6a757cbd025ccfda7b78
Rodrigo César <rodrigocnn@gmail.com>
2025-07-05 12:53:53 -0300

    test(trip/factory): add mocked name of status

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     end_date { start_date + rand(1..5).days }
     origin { Faker::Address.city }
     destination { Faker::Address.city }
-    status { Trip.statuses.keys.sample }  # Considerando enum com nomes como string
+    status { "planejada" }
     driver_id { Driver.pluck(:id).sample || create(:driver).id }
     vehicle_id { Vehicle.pluck(:id).sample || create(:vehicle).id }
   end
