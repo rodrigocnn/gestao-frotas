f358edf4122a44af87237b4036d392195e99aa26
Rodrigo César <rodrigocnn@gmail.com>
2025-07-05 12:50:11 -0300

    feat(user): add callback generate_jti to make jti random

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-
   include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  before_create :generate_jti
+
+  private
+
+  def generate_jti
+    self.jti = SecureRandom.uuid if self.jti.blank?
+  end
 end
