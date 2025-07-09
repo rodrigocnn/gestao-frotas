d0c9c1f1ae416d0adaa4513ab14a2ed8efb8d692
Rodrigo César <rodrigocnn@gmail.com>
2025-07-09 18:03:14 -0300

    test(trips) update trips model test

--- spec/models/trips_spec.rb
+++ spec/models/trips_spec.rb
@@ -1,37 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "Trips", type: :model  do
-it "cria trip com veículo disponível usando build + save" do
-  vehicle = create(:vehicle, status: :disponivel)
-  driver = create(:driver)
+RSpec.describe Trip, type: :model do
+  let(:vehicle) { create(:vehicle, status: :disponivel) }
+  let(:driver) { create(:driver) }
 
-  trip = build(:trip, vehicle: vehicle, driver: driver)
+  describe "Criação de viagem" do
+    it "cria trip com veículo disponível" do
+      trip = create(:trip, vehicle: vehicle, driver: driver)
+      expect(trip).to be_valid
+    end
 
-  expect(trip).to be_valid
-end
-
-
-
-
-
-
-  it "should set status em_uso after create" do
-    vehicle = create(:vehicle, status: :disponivel)
-    driver = create(:driver)
-
-    trip = build(:trip, vehicle: vehicle, driver: driver)
-
-    expect(trip).to be_valid
-    trip.save!
-
-    vehicle.reload
-    expect(vehicle.status).to eq("em_uso")
+    it "define status do veículo como em_uso após criar trip" do
+      create(:trip, vehicle: vehicle, driver: driver)
+      expect(vehicle.status).to eq("em_uso")
+    end
   end
 
-   context "Validations" do
-    it "does not allow blank start_date" do
-         trip = build(:trip, start_date: nil)
-         expect(trip).to_not be_valid
-      end
+  describe "Finalização de viagem" do
+    it "atualiza status do veículo para disponivel se trip for finalizada" do
+      trip = create(:trip, vehicle: vehicle, driver: driver)
+      trip.update!(status: :finalizada)
+      expect(vehicle.reload.status).to eq("disponivel")
     end
+  end
+
+  describe "Validações" do
+    it "não permite start_date em branco" do
+      trip = build(:trip, start_date: nil)
+      expect(trip).to_not be_valid
+      expect(trip.errors[:start_date]).to include("can't be blank")
+    end
+  end
 end
